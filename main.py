798bca6a9b8225cce1bd91d4a87d4cf5d401ba05
LucasBatista7 <147443802+LucasBatista7@users.noreply.github.com>
2023-11-13 17:24:46 -0300

    Update main.py
    Testado e corrigido os erros

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import pyautogui #detecta eventos/acoes
 import keyboard #parte do mouse
+
+freq = float(input("Enter frequency (in seconds): "))
+print("Task will start in 3 seconds")
+pyautogui.sleep(3)
 while not keyboard.is_pressed('m'): #Enquanto nao cancelar...
-  pyautogui.sleep(5)
-  pyautogui.press('down')
-  pyautogui.sleep(0.016)
-  pyautogui.press('left')
+    pyautogui.sleep(freq)
+    pyautogui.press('down')
+    pyautogui.press('left')
+print('Ended task')
